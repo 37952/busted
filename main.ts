326080bc9bae7fffd65df82e302e3c5aa1e2c19d
37952 <edwardfan3737@gmail.com>
2024-11-16 09:47:19 -0800

    v5

--- main.ts
+++ main.ts
@@ -4,6 +4,17 @@ namespace SpriteKind {
     export const m = SpriteKind.create()
     export const all = SpriteKind.create()
 }
+function set_level () {
+    for (let index = 0; index <= level - 1; index++) {
+        create_bounce()
+    }
+}
+sprites.onDestroyed(SpriteKind.all, function (sprite) {
+    list = sprites.allOfKind(SpriteKind.all)
+    if (list.length == 0) {
+        game.splash("LEVEL " + level, "COMPLETED")
+    }
+})
 function create_all (mBall: Sprite, go_left: boolean) {
     ball = sprites.create(img`
         . . . . . . . . . . . . . . . 
@@ -199,6 +210,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.bounce, function (sprite, ot
     create_s(otherSprite, true)
     create_s(otherSprite, false)
     sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.s, function (sprite, otherSprite) {
     create_bounce()
@@ -233,6 +245,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.m, function (sprite, otherSp
     create_all(otherSprite, true)
     create_all(otherSprite, true)
     sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
 })
 function create_m (sBall: Sprite, goLeft: boolean) {
     ball = sprites.create(img`
@@ -267,10 +280,12 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.s, function (sprite, otherSp
     create_m(otherSprite, true)
     create_m(otherSprite, false)
     sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.all, function (sprite, otherSprite) {
     sprites.destroy(sprite)
     sprites.destroy(otherSprite, effects.hearts, 500)
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.m, function (sprite, otherSprite) {
     create_bounce()
@@ -278,9 +293,11 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.m, function (sprite, otherSprite
     info.changeLifeBy(-1)
 })
 let projectile: Sprite = null
-let list: Sprite[] = []
 let ball: Sprite = null
+let list: Sprite[] = []
 let mySprite: Sprite = null
+let level = 0
+level = 1
 scene.setBackgroundColor(1)
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -303,7 +320,7 @@ mySprite = sprites.create(img`
 mySprite.bottom = 100
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite, 100, 0)
-create_bounce()
+set_level()
 game.onUpdate(function () {
     list = sprites.allOfKind(SpriteKind.Projectile)
     for (let value of list) {
